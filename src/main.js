57e7cee85914d3f2100268c9554d7b11f169f936
that-jamal <varg.jamil@gmail.com>
2024-08-22 14:35:51 +0300

    update js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import './style.css'
-const images= [
+import './style.css';
+
+const images = [
     'public/truck-1.jpg',
     'public/truck-2.jpg',
     'public/truck-3.jpg',
@@ -10,22 +11,25 @@ const images= [
     'public/truck-8.jpg'
 ];
 
-const imgElement = document.getElementById('slideshow') as HTMLImageElement;
-const prevButton = document.getElementById('prev') as HTMLButtonElement;
-const nextButton = document.getElementById('next') as HTMLButtonElement;
+const imgElement = document.getElementById('slideshow');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
 let currentIndex = 0;
+
 const updateImage = () => {
     imgElement.src = images[currentIndex];
 };
+
 const showPreviousImage = () => {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     updateImage();
 };
+
 const showNextImage = () => {
     currentIndex = (currentIndex + 1) % images.length;
     updateImage();
 };
+
 prevButton.addEventListener('click', showPreviousImage);
 nextButton.addEventListener('click', showNextImage);
 updateImage();
-/**made by jamil varg 2024 sosdata lia**/
